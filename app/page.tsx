7b14dd9498d49d7e14510359f2576b28b9146766
kalsariyadarshan <142231808+kalsariyadarshan@users.noreply.github.com>
2024-11-10 22:51:42 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -139,7 +139,7 @@ export default function Chatbot() {
             type="submit"
             className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
           >
-            Send
+            Send Message
           </button>
         </form>
       </div>
